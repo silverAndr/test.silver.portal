a3e343b842d98b83d889c84aa159d0373ad8a10d
silver <a.serebryakov@2vgroup.ru>
2020-02-03 23:28:38 +0300

    include files

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,11 +1,24 @@
 <?php
 
+use Bitrix\Main\Context;
+use Bitrix\Main\EventManager;
+
+//require(__DIR__ . '/test01/disk.php'); // пока отключил облако тегов ошибка с объектом $USER
+require(__DIR__ . '/test01/js.php');
+require(__DIR__ . '/test01/lib/Ref.php');
+require(__DIR__ . '/test01/lib/DocumentTags.php');
+require(__DIR__ . '/test01/lib/Ajax.php');
+
+$eventManager = EventManager::getInstance();
+// $eventManager->addEventHandlerCompatible('main', 'onAfterEpilog', 'dumpViews');
+// $eventManager->addEventHandlerCompatible('search', "BeforeIndex", "addTagToIndex");
+
 /**
  * Форматированный print_r
  * @param $data
  */
 function dump($data) {
-	print "<pre>";
+	 print "<pre>";
 	print_r($data);
-	print "</pre>";
+	 print "</pre>";
 }
